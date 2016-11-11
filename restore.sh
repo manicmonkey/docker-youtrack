19c67b352ff2d7619ad3f683cb9b453dbdec1ae4
James Baxter <j.w.baxter@gmail.com>
2016-11-11 18:52:37 +0100

    Update image names

--- restore.sh
+++ restore.sh
@@ -1,2 +1,2 @@
-docker run --rm --volumes-from youtrack_data -v $(pwd):/backup manicmonkey/docker-youtrack bash -c "cd /data && rm -rf * && tar zxvf /backup/backup.tar.gz"
+docker run --rm --volumes-from youtrack_data -v $(pwd):/backup manicmonkey/youtrack:7 bash -c "cd / && rm -rf data && tar zxvf /backup/backup.tar.gz"
 
